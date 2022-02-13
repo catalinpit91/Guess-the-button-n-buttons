5cdc74fbcb0e04fb61fbed810b17963b1903bde0
catalinpit91 <92463435+catalinpit91@users.noreply.github.com>
2022-02-13 13:32:36 +0200

    js code without jquery

--- javascript.js
+++ javascript.js
@@ -3,8 +3,7 @@
   function addButtons() {
     var buttonsNumber = document.getElementById("numberOfButtons").value;
       for (let i = 0; i < buttonsNumber; ++i) {
-        $('#Buttons').append(`        
-          <a href="#" id = "`+ i +`" class="btn btn-primary" onclick="return checkWinnerButton(` + i + `);">choose a button</a>`);
+        document.getElementById("Buttons").innerHTML += '<button type = "button" id = "`+ i +`" class="btn btn-primary" onclick="return checkWinnerButton(` + i + `);">choose a button</button>';
       }
      return false;
   }
